44f32b49e9e3b3aefbc29391ec06c09db34206ba
Akshith985 <akshithclg@gmail.com>
2025-08-10 22:13:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.getElementById("splash-screen").style.display = "none";
+        document.getElementById("main-content").style.display = "block";
+    }, 3000); // show splash for 3 seconds
+});
 
